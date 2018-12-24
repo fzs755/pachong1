3000511a730124a16a7a4c5e6930e0b750289cd0
fzs755 <fzs755@163.com>
2018-12-24 17:47:26 +0800

    12-24

--- 4.py
+++ 4.py
@@ -6,19 +6,24 @@ import sys
 
 y=[]
 f = open('c.txt','r',encoding='utf-8')
-y =f.readlines()
+y = f.readlines()
 for x in y:
   #  print (x)
   response = request.urlopen(x)
   html_doc = response.read()
   soup = BeautifulSoup(html_doc,"html.parser")
   for link in soup.find_all('a', attrs={"target":"_blank"}):
-    l=link.get('href')
+    x=x.strip()
+    l=link.get('href').replace("/article/sqfb","",1)
     t=link.get_text()
-    #print(t)
-    x=x+l+t
-    with open ('e.txt','a+', encoding='utf-8') as g:
-      g.write(x)
+    z=""
+    #print(l)P
+    if (l.find("2018")!=-1):
+      z=x+l+t
+      #print(z)
+    with open ('d.txt','a+', encoding='utf-8') as g:
+      if (z!=""):
+        g.write(z+'\n')
   #  if(l+x1):
    #   soup1=BeautifulSoup(request.urlopen(l+x1),"html.parser")
     #  for link1 in soup1.find_all('a', attrs={"target":"_blank"}):
